c32bf2b63d455a03f3596c41979cd88307ed163d
Quintin de Kok <quintindk@tangentsolutions.co.za>
2023-04-21 08:07:13 +0200

    fix: app config sku

--- terraform/app_config.tf
+++ terraform/app_config.tf
@@ -2,4 +2,5 @@ resource "azurerm_app_configuration" "app_conf" {
   name                = "appconf-${var.base_name}-${var.environment}-${format("%02s",var.base_instance)}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
+  sku                 = "standard"
 }
